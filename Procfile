1d34b1fca81ed32914f5a234badc9703bb311b94
unknown <abdulbasitadio@gmail.com>
2022-05-30 12:26:20 +0100

    heroku

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: gunicorn -b "0.0.0.0:$PORT" -w 3 blogass.wsgi
+release: python manage.py migrate
--- blogapp/settings.py
+++ blogapp/settings.py
@@ -11,8 +11,11 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-from dotenv import load_dotenv , find_dotenv
-load_dotenv(find_dotenv())
+# import os
+# from dotenv import load_dotenv, find_dotenv
+
+# load_dotenv(find_dotenv())
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,13 +25,19 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
-
+# SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
+SECRET_KEY = 'django-insecure-1^fcgdujy^wi4n=92f$rn6(hi58!t=bdxife1n5vbhrbppqe#v'
+enviroment = os.getenv('ENVIROMENT', 'development')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+if enviroment == "development":
 
-ALLOWED_HOSTS = []
+  DEBUG = True
+  ALLOWED_HOSTS = []
 
+elif enviroment == "productions":
+
+    DEBUG = False
+    ALLOWED_HOSTS = ['basitblog.herokuapp.com/']
 
 # Application definition
 
@@ -38,12 +47,15 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
     'blogass',
+    # 'taggit',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -119,6 +131,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+sTATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.compressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
